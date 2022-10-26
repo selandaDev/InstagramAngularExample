cc5ca4ef90317bdadf497567c839511a7c40dc46
Francisco Mañosa <fmanosa23@gmail.com>
2022-10-26 17:03:07 +0200

    First Commit
      	modified:   angular.json
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/app/app.component.html
    	modified:   src/app/app.component.ts
    	modified:   src/app/app.module.ts
    	new file:   src/assets/1.jpg
    	new file:   src/assets/10.jpg
    	new file:   src/assets/11.jpg
    	new file:   src/assets/12.jpg
    	new file:   src/assets/13.jpg
    	new file:   src/assets/14.jpg
    	new file:   src/assets/15.jpg
    	new file:   src/assets/16.jpg
    	new file:   src/assets/2.jpg
    	new file:   src/assets/3.jpg
    	new file:   src/assets/4.jpg
    	new file:   src/assets/5.jpg
    	new file:   src/assets/6.jpg
    	new file:   src/assets/7.jpg
    	new file:   src/assets/8.jpg
    	new file:   src/assets/9.jpg
    	new file:   src/assets/instagram.svg
    	new file:   src/assets/instagramLogo.png
    	modified:   src/index.html
    	modified:   src/styles.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import SwiperCore, { SwiperOptions } from 'swiper';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,102 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'instaangular';
+
+  config: SwiperOptions = {
+    slidesPerView: 5,
+    spaceBetween: 10,
+    pagination: { clickable: true },
+  };
+
+  items = [
+    {
+      foto : './assets/1.jpg',
+      titulo : 'un titulo'
+    },
+    {
+      foto : './assets/2.jpg',
+      titulo : 'un titulo'
+    },
+    {
+      foto : './assets/3.jpg',
+      titulo : 'un titulo'
+    },
+    {
+      foto : './assets/4.jpg',
+      titulo : 'un titulo'
+    },
+    {
+      foto : './assets/5.jpg',
+      titulo : 'un titulo'
+    },
+    {
+      foto : './assets/6.jpg',
+      titulo : 'un titulo'
+    },
+    {
+      foto : './assets/7.jpg',
+      titulo : 'un titulo'
+    },
+    {
+      foto : './assets/8.jpg',
+      titulo : 'un titulo'
+    },
+    {
+      foto : './assets/9.jpg',
+      titulo : 'un titulo'
+    },
+    {
+      foto : './assets/10.jpg',
+      titulo : 'un titulo'
+    },
+    {
+      foto : './assets/11.jpg',
+      titulo : 'un titulo'
+    },
+    {
+      foto : './assets/12.jpg',
+      titulo : 'un titulo'
+    },    
+    {
+      foto : './assets/13.jpg',
+      titulo : 'un titulo'
+    },
+    {
+      foto : './assets/14.jpg',
+      titulo : 'un titulo'
+    },
+    {
+      foto : './assets/15.jpg',
+      titulo : 'un titulo'
+    },
+    {
+      foto : './assets/16.jpg',
+      titulo : 'un titulo'
+    },
+    {
+      foto : './assets/1.jpg',
+      titulo : 'un titulo'
+    },
+    {
+      foto : './assets/2.jpg',
+      titulo : 'un titulo'
+    },
+    {
+      foto : './assets/3.jpg',
+      titulo : 'un titulo'
+    },
+    {
+      foto : './assets/2.jpg',
+      titulo : 'un titulo'
+    },
+    {
+      foto : './assets/3.jpg',
+      titulo : 'un titulo'
+    }
+
+  ]
+
+
+
+
 }
